2e578a298fc7fa4b2e0c5d93ce0438cacaaa3d17
insideout-oss <irina.bezruk@gmail.com>
2021-07-18 23:18:18 +0300

    Read test app info from config file

--- conftest.py
+++ conftest.py
@@ -1,20 +1,25 @@
-
+import json
+import os.path
 import pytest
+
 from fixture.application import Application
 
 fixture = None
+target = None
 
 @pytest.fixture
 def app(request):
     global fixture
+    global target
     browser = request.config.getoption("--browser")
-    base_url = request.config.getoption("--base_url")
-    if fixture is None:
-        fixture = Application(browser=browser, base_url=base_url)
-    else:
-        if not fixture.is_valid():
-            fixture = Application(browser=browser, base_url=base_url)
-    fixture.session.ensure_login("admin", "secret")
+    if target is None:
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
+        with open(config_file) as f:
+            target = json.load(f)
+    if fixture is None or not fixture.is_valid():
+        fixture = Application(browser=browser, base_url=target['baseUrl'])
+
+    fixture.session.ensure_login(target['username'], target['password'])
     return fixture
 
 
@@ -26,6 +31,7 @@ def stop(request):
     request.addfinalizer(fin)
     return fixture
 
+
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
-    parser.addoption("--base_url", action="store", default="http://localhost/addressbook/")
+    parser.addoption("--target", action="store", default="target.json")
